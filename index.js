ef00fbedb4ce080dcde1e6c86ee1c1b6f1cd696a
synchor020 <trandatkmatt@gmail.com>
2023-04-08 22:20:11 +0700

    ok

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+
+//Set static folder
+app.use(express.static(path.join(__dirname,"public")));
+
+
+
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
+
